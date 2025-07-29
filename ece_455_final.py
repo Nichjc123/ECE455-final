f6680220891b9c0111602484b133fed124c86af4
Nicholas Cantone <ncantone@uwaterloo.ca>
2025-07-29 15:54:25 -0400

    creating set of tasks from input

--- ece_455_final.py
+++ ece_455_final.py
@@ -1,15 +1,22 @@
 import sys
+# Deadline monotonic scheduling: Assign the priority based on time to deadline
+
+# Storing set of task tuples 
+# (execution time, period, deadline)
+task_set = []
 
 def main():
-    #HANDLE FILE INPUT
+    # Handling file input
     filename = sys.argv[1]
 
     try:
         with open(filename, 'r') as file:
-            content = file.read()
-            print(content)
+            for line in file.readlines():
+                # Create tuple of (e, p, d) and add to task set
+                task_set.append(tuple(line.replace("\n", "").split(',')))
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
-    
+
+
 if __name__ == "__main__":
     main()
