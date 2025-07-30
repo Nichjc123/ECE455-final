0834963255f59d2c27ac605946f8cb9100bfc795
Nicholas Cantone <ncantone@uwaterloo.ca>
2025-07-30 10:21:28 -0400

    calculating release times and finding hyperperiod

--- ece_455_final.py
+++ ece_455_final.py
@@ -1,11 +1,20 @@
 import sys
-# Deadline monotonic scheduling: Assign the priority based on time to deadline
+import numpy as np
 
 # Storing set of task tuples 
 # (execution time, period, deadline)
 task_set = []
 
+# (time) - we can know which task(s) it corresponds to by seing if its divisible by period
+release_times = []
+
+# index of current task on CPU
+currently_running_task = -1
+
 def main():
+    # for calculating hyperperiod
+    periods = []
+
     # Handling file input
     filename = sys.argv[1]
 
@@ -13,9 +22,26 @@ def main():
         with open(filename, 'r') as file:
             for line in file.readlines():
                 # Create tuple of (e, p, d) and add to task set
-                task_set.append(tuple(line.replace("\n", "").split(',')))
+                curr_task = tuple(map(int, line.strip().split(',')))
+                task_set.append(curr_task)
+                periods.append(int(curr_task[1]))
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
+
+    # Create an array of scheduling points, not including task finishing (up to hyperperiod)
+    hyperperiod = np.lcm.reduce(periods)
+
+    # Find set of all release times
+    points = set()
+    for i in range(len(task_set)):
+        curr = 0
+        while (curr <= hyperperiod):
+            if curr not in points:
+                points.add(curr)
+                release_times.append(curr)
+            curr += task_set[i][1]
+    
+    release_times.sort()
 
 
 if __name__ == "__main__":
